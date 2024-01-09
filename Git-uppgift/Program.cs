ff3da6fae302127953fa0df09050e52b920d629c
Tobias Lidzén <tobias@axevalla.fhsk.se>
2024-01-09 13:33:32 +0100

    Redo att delas ut

--- Git-uppgift/Program.cs
+++ Git-uppgift/Program.cs
@@ -4,188 +4,194 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Välkommen till multiprogrammet som gör det mesta. Välj vilket delprogram du vill köra");
-        Console.WriteLine("1. Räkna ut hypotenusa");
-        Console.WriteLine("2. Räkna ut en rektangels area");
-        Console.WriteLine("3. Räkna ut medelvärdet av en lista");
-        Console.WriteLine("4. Är en sträng/mening ett palindrom?");
-        Console.WriteLine("5. Skapa delay på x antal sekunder");
-        Console.WriteLine("6. Sortera några ord i bokstavsordning");
-        Console.WriteLine("7. Kolla om ett tal är ett primtal");
-        Console.WriteLine("8. Kolla vilka primtal som finns upp till ett visst tal");
-        Console.WriteLine("9. Testa om ett nummer är 'PERFEKT'");
-        Console.WriteLine("10. Vad blir ett tal i fakultet?");
-        Console.WriteLine("11. Kryptera en text med ceasarkryptering");
-        Console.WriteLine("12. Dekryptera en text som använder ceasarkryptering");
-
-        Console.Write("Skriv in ditt val: ");
-        int val = int.Parse(Console.ReadLine());
-
-        switch (val)
+        int val = 1;
+        while (val > 0)
         {
-            case 1:
-                {
-                    float bas;
-                    float hojd;
-                    float hypo;
-                    Console.WriteLine("Du vill räkna ut hypotenusan på en triangel. Skriv in bas och höjd för att få reda på hypotenusan.");
-                    Console.Write("Skriv in bas: ");
-                    bas = float.Parse(Console.ReadLine());
-                    Console.Write("Skriv in hojd: ");
-                    hojd = float.Parse(Console.ReadLine());
-                    hypo = FuncHypotenusa(bas, hojd);
-                    Console.WriteLine("Hypotenusan är: " + hypo);
-                    break;
-                }
-            case 2:
-                {
-                    float sida1;
-                    float sida2;
-                    float area;
-                    Console.WriteLine("Vill du veta arean av en rektangel? Fyll i hur långa sidorna i nedan och låt funktionen göra jobbet");
-                    Console.Write("Skriv in första sidans längd: ");
-                    sida1 = float.Parse(Console.ReadLine());
-                    Console.Write("Skriv in andra sidans längd: ");
-                    sida2 = float.Parse(Console.ReadLine());
-                    area = FuncRArea(sida1, sida2);
-                    Console.WriteLine("Rektangelns area är: " + area);
-                    break;
-                }
-            case 3:
-                {
-                    int[] lista = new int[100];
-                    int antalnr;
-                    float svar;
-                    Console.WriteLine("Du söker medelvärdet av en massa värden. Följ instruktionerna i programmet och låt funktionen göra sin magic.");
-                    Console.Write("Skriv in hur många nummer du vill använda att få ut medelvärde för: ");
-                    antalnr = int.Parse(Console.ReadLine());
-                    for (int i = 0; i < antalnr; i++)
+            Console.WriteLine("Välkommen till multiprogrammet som gör det mesta. Välj vilket delprogram du vill köra");
+            Console.WriteLine("1. Räkna ut hypotenusan på en triangel");
+            Console.WriteLine("2. Räkna ut en rektangels area");
+            Console.WriteLine("3. Räkna ut medelvärdet av en lista med värden");
+            Console.WriteLine("4. Är en sträng/mening ett palindrom?");
+            Console.WriteLine("5. Skapa delay på x antal sekunder");
+            Console.WriteLine("6. Sortera några ord i bokstavsordning");
+            Console.WriteLine("7. Kolla om ett tal är ett primtal");
+            Console.WriteLine("8. Kolla vilka primtal som finns upp till ett visst tal");
+            Console.WriteLine("9. Testa om ett nummer är 'PERFEKT'");
+            Console.WriteLine("10. Vad blir ett tal i fakultet?");
+            Console.WriteLine("11. Kryptera en text med ceasarkryptering");
+            Console.WriteLine("12. Dekryptera en text som använder ceasarkryptering");
+            Console.WriteLine("0. Avsluta programmet");
+
+
+            Console.Write("Skriv in ditt val: ");
+            val = int.Parse(Console.ReadLine());
+
+            switch (val)
+            {
+                case 1:
                     {
-                        Console.Write("Skriv in värde " + i + ": ");
-                        lista[i] = int.Parse(Console.ReadLine());
+                        float bas;
+                        float hojd;
+                        float hypo;
+                        Console.WriteLine("Du vill räkna ut hypotenusan på en triangel. Skriv in bas och höjd för att få reda på hypotenusan.");
+                        Console.Write("Skriv in bas: ");
+                        bas = float.Parse(Console.ReadLine());
+                        Console.Write("Skriv in hojd: ");
+                        hojd = float.Parse(Console.ReadLine());
+                        hypo = FuncHypotenusa(bas, hojd);
+                        Console.WriteLine("Hypotenusan är: " + hypo);
+                        break;
                     }
-                    svar = FuncMedelvarde(lista, antalnr);
-                    Console.WriteLine("Medelvärdet är: " + svar);
-                    break;
-                }
-            case 4:
-                {
-                    string palTest;
-                    Console.WriteLine("Är en mening eller ett ord ett palindrom? Testa och skriv ett ord och se resultatet!");
-                    Console.Write("Skriv ordet/meningen du vill testa om det är ett palindrom: ");
-                    palTest = Console.ReadLine();
-                    if (FuncIsPalindrom(palTest))
+                case 2:
                     {
-                        Console.WriteLine(palTest + " IS A FRICKIN' PALINDROME, MAN!");
+                        float sida1;
+                        float sida2;
+                        float area;
+                        Console.WriteLine("Vill du veta arean av en rektangel? Fyll i hur långa sidorna i nedan och låt funktionen göra jobbet");
+                        Console.Write("Skriv in första sidans längd: ");
+                        sida1 = float.Parse(Console.ReadLine());
+                        Console.Write("Skriv in andra sidans längd: ");
+                        sida2 = float.Parse(Console.ReadLine());
+                        area = FuncRArea(sida1, sida2);
+                        Console.WriteLine("Rektangelns area är: " + area);
+                        break;
                     }
-                    else
+                case 3:
                     {
-                        Console.WriteLine("No luck.. " + palTest + " is no palindrome");
+                        int[] lista = new int[100];
+                        int antalnr;
+                        float svar;
+                        Console.WriteLine("Du söker medelvärdet av en massa värden. Följ instruktionerna i programmet och låt funktionen göra sin magic.");
+                        Console.Write("Skriv in hur många nummer du vill använda att få ut medelvärde för: ");
+                        antalnr = int.Parse(Console.ReadLine());
+                        for (int i = 0; i < antalnr; i++)
+                        {
+                            Console.Write("Skriv in värde " + i + ": ");
+                            lista[i] = int.Parse(Console.ReadLine());
+                        }
+                        svar = FuncMedelvarde(lista, antalnr);
+                        Console.WriteLine("Medelvärdet är: " + svar);
+                        break;
                     }
-                    break;
-                }
-            case 5:
-                {
-                    string ord;
-                    int sek;
-                    Console.WriteLine("Så det finns en funktion som skapar delay? Ja, om nån har programmerat den! prova och skriv nåt å se om det blir delay.");
-                    Console.Write("Skriv in det ord du vill ska skrivas ut med delay: ");
-                    ord = Console.ReadLine();
-                    Console.Write("Skriv in hur många sekunder du vill att delayen ska vara: ");
-                    sek = int.Parse(Console.ReadLine());
-                    FuncDelay(sek);
-                    Console.WriteLine(ord);
-                    break;
-                }
-            case 6:
-                {
-                    string ord;
-                    string sorteratOrd;
-                    Console.WriteLine("Sortera en sträng i bokstavsordning. Skriv '-' (ett bindestreck) mellan orden du vill sortera");
-                    Console.WriteLine("Exempel: hej-yggdrasil-tomte-bonde");
-                    Console.Write("Skriv in det du vill sortera i bokstavsordning: ");
-                    ord = Console.ReadLine();
-                    sorteratOrd = FuncSortera(ord);
-                    Console.WriteLine("Först skrev du in: " + ord);
-                    Console.WriteLine("Det blev till: " + sorteratOrd);
-                    break;
-                }
-            case 7:
-                {
-                    int tal;
-                    Console.WriteLine("Du vill prova om ett tal är primtal? Prova på!");
-                    Console.Write("Skriv in ett tal och se om det är ett primtal: ");
-                    tal = int.Parse(Console.ReadLine());
-                    Console.WriteLine(tal + " är ett primtal? ");
-                    Console.WriteLine(FuncIsPrime(tal));
-                    break;
-                }
-            case 8:
-                {
-                    int varde;
-                    Console.WriteLine("Dags att prova att få en lista med massa jäkla primtal upp till ett visst värde.");
-                    Console.Write("Skriv in hur högt värde du vill hitta alla primtal upp till: ");
-                    varde = int.Parse(Console.ReadLine());
-                    int[] allPrimes;
-                    allPrimes = FuncHittaPrimtal(varde);
-                    Console.WriteLine("Denna lista som skrivs ut nedan är alla primtal upp till det tal du skrev in: ");
-                    for (int i = 0; i < allPrimes.Length; i++)
+                case 4:
                     {
-                        Console.Write(allPrimes[i] + ", ");
+                        string palTest;
+                        Console.WriteLine("Är en mening eller ett ord ett palindrom? Testa och skriv ett ord och se resultatet!");
+                        Console.Write("Skriv ordet/meningen du vill testa om det är ett palindrom: ");
+                        palTest = Console.ReadLine();
+                        if (FuncIsPalindrom(palTest))
+                        {
+                            Console.WriteLine(palTest + " IS A FRICKIN' PALINDROME, MAN!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No luck.. " + palTest + " is no palindrome");
+                        }
+                        break;
                     }
-                    break;
-                }
-            case 9:
-                {
-                    int tal;
-                    Console.WriteLine("Testa om ett nummer är perfekt!");
-                    Console.Write("Skriv in det tal du vill testa: ");
-                    tal = int.Parse(Console.ReadLine());
-                    Console.WriteLine(FuncPerfectNum(tal));
-                    break;
-                }
-            case 10:
-                {
-                    int tal;
-                    int svar;
-                    Console.Write("Skriv in det tal du vill prova i fakultet: ");
-                    tal = int.Parse(Console.ReadLine());
-                    svar = FuncFakultet(tal);
-                    Console.WriteLine(tal + " i fakultet blir: " + svar);
-                    break;
-                }
-            case 11:
-                {
-                    int chiffer;
-                    string text;
-                    string kryptad;
-                    Console.WriteLine("Kryptera en text! Skriv in chiffer (ett heltal mellan 1-20): ");
-                    chiffer = int.Parse(Console.ReadLine());
-                    Console.Write("Skriv in texten du vill kryptera: ");
-                    text = Console.ReadLine();
-                    kryptad = FuncCeasarCrypt(text, chiffer);
-                    Console.WriteLine("Din text krypterad: " + kryptad);
-                    break;
-                }
-            case 12:
-                {
-                    int chiffer;
-                    string text;
-                    string decrypt;
-                    Console.WriteLine("Decrypt dax. Du behöver dels den krypterade strängen och dess chiffer");
-                    Console.Write("Skriv in det chiffer som användes vid kryptering (ett heltal mellan 1-20): ");
-                    chiffer = int.Parse(Console.ReadLine());
-                    Console.Write("Skriv in texten du vill dekryptera: ");
-                    text = Console.ReadLine();
-                    decrypt = FuncCeasarDecrypt(text, chiffer);
-                    // Implementera din dekrypteringslogik här
-                    break;
-                }
-            default:
-                {
-                    Console.WriteLine("Whatever");
-                    break;
-                }
+                case 5:
+                    {
+                        string ord;
+                        int sek;
+                        Console.WriteLine("Så det finns en funktion som skapar delay? Ja, om nån har programmerat den! prova och skriv nåt å se om det blir delay.");
+                        Console.Write("Skriv in det ord du vill ska skrivas ut med delay: ");
+                        ord = Console.ReadLine();
+                        Console.Write("Skriv in hur många sekunder du vill att delayen ska vara: ");
+                        sek = int.Parse(Console.ReadLine());
+                        FuncDelay(sek);
+                        Console.WriteLine(ord);
+                        break;
+                    }
+                case 6:
+                    {
+                        string ord;
+                        string sorteratOrd;
+                        Console.WriteLine("Sortera en sträng i bokstavsordning. Skriv '-' (ett bindestreck) mellan orden du vill sortera");
+                        Console.WriteLine("Exempel: hej-yggdrasil-tomte-bonde");
+                        Console.Write("Skriv in det du vill sortera i bokstavsordning: ");
+                        ord = Console.ReadLine();
+                        sorteratOrd = FuncSortera(ord);
+                        Console.WriteLine("Först skrev du in: " + ord);
+                        Console.WriteLine("Det blev till: " + sorteratOrd);
+                        break;
+                    }
+                case 7:
+                    {
+                        int tal;
+                        Console.WriteLine("Du vill prova om ett tal är primtal? Prova på!");
+                        Console.Write("Skriv in ett tal och se om det är ett primtal: ");
+                        tal = int.Parse(Console.ReadLine());
+                        Console.WriteLine(tal + " är ett primtal? ");
+                        Console.WriteLine(FuncIsPrime(tal));
+                        break;
+                    }
+                case 8:
+                    {
+                        int varde;
+                        Console.WriteLine("Dags att prova att få en lista med massa jäkla primtal upp till ett visst värde.");
+                        Console.Write("Skriv in hur högt värde du vill hitta alla primtal upp till: ");
+                        varde = int.Parse(Console.ReadLine());
+                        int[] allPrimes;
+                        allPrimes = FuncHittaPrimtal(varde);
+                        Console.WriteLine("Denna lista som skrivs ut nedan är alla primtal upp till det tal du skrev in: ");
+                        for (int i = 0; i < allPrimes.Length; i++)
+                        {
+                            Console.Write(allPrimes[i] + ", ");
+                        }
+                        break;
+                    }
+                case 9:
+                    {
+                        int tal;
+                        Console.WriteLine("Testa om ett nummer är perfekt!");
+                        Console.Write("Skriv in det tal du vill testa: ");
+                        tal = int.Parse(Console.ReadLine());
+                        Console.WriteLine(FuncPerfectNum(tal));
+                        break;
+                    }
+                case 10:
+                    {
+                        int tal;
+                        int svar;
+                        Console.Write("Skriv in det tal du vill prova i fakultet: ");
+                        tal = int.Parse(Console.ReadLine());
+                        svar = FuncFakultet(tal);
+                        Console.WriteLine(tal + " i fakultet blir: " + svar);
+                        break;
+                    }
+                case 11:
+                    {
+                        int chiffer;
+                        string text;
+                        string kryptad;
+                        Console.WriteLine("Kryptera en text! Skriv in chiffer (ett heltal mellan 1-20): ");
+                        chiffer = int.Parse(Console.ReadLine());
+                        Console.Write("Skriv in texten du vill kryptera: ");
+                        text = Console.ReadLine();
+                        kryptad = FuncCeasarCrypt(text, chiffer);
+                        Console.WriteLine("Din text krypterad: " + kryptad);
+                        break;
+                    }
+                case 12:
+                    {
+                        int chiffer;
+                        string text;
+                        string decrypt;
+                        Console.WriteLine("Decrypt dax. Du behöver dels den krypterade strängen och dess chiffer");
+                        Console.Write("Skriv in det chiffer som användes vid kryptering (ett heltal mellan 1-20): ");
+                        chiffer = int.Parse(Console.ReadLine());
+                        Console.Write("Skriv in texten du vill dekryptera: ");
+                        text = Console.ReadLine();
+                        decrypt = FuncCeasarDecrypt(text, chiffer);
+                        // Implementera din dekrypteringslogik här
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Välkommen tillbaka");
+                        break;
+                    }
+            }
         }
     }
     static float FuncHypotenusa(float bas, float hojd)
