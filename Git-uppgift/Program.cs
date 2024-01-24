03b89713f850f587931292f640e38cb4658afe6c
Erik Strängby <h22erst@axevalla.fhsk.se>
2024-01-24 11:52:35 +0100

    funktion 2 klar

--- Git-uppgift/Program.cs
+++ Git-uppgift/Program.cs
@@ -207,7 +207,9 @@ class Program
         float area = 0;
         // Gör en funktion som räknar ut area av en rektangel med värdena som fås av användaren.
         // Returnera arean
+        area = bas * hojd;
         return area;
+       
     }
 
     static float FuncMedelvarde(int[] listaMedVarden, int antal)
